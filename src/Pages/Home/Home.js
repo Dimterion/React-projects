2f9ea6844821d057a90d1dd3df4084512ac3d215
Dimterion <dimterion59@gmail.com>
2022-12-08 15:20:37 +0100

    Image and link corrections.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -12,9 +12,12 @@ function Home() {
         <h1>React projects</h1>
         <h2>
           Here are several React projects I've built while doing React course on{" "}
-          <a href="https://scrimba.com/">Scrimba</a>.
+          <a href="https://scrimba.com/" target="_blank" rel="noreferrer">
+            Scrimba
+          </a>
+          .
         </h2>
-        <h3>The app was built with React and React Router.</h3>
+        <h3>This site was created with React and React Router.</h3>
         <p>
           You can navigate through the projects on top of each page or using the
           links below on this page.
@@ -22,11 +25,17 @@ function Home() {
       </div>
       <div className="home-links">
         <Link to="/business-card">
-          <img src={businessCardImg} alt="Business Card section cover"/>
+          <img src={businessCardImg} alt="Business Card section cover" />
         </Link>
-        <Link to="/travel-journal"><img src={travelJournalImg} alt="Travel Journal section cover"/></Link>
-        <Link to="/meme-generator"><img src={memeGeneratorImg} alt="Meme Generator section cover"/></Link>
-        <Link to="/tenzies-game"><img src={tenziesGameImg} alt="Tenzies Game section cover"/></Link>
+        <Link to="/travel-journal">
+          <img src={travelJournalImg} alt="Travel Journal section cover" />
+        </Link>
+        <Link to="/meme-generator">
+          <img src={memeGeneratorImg} alt="Meme Generator section cover" />
+        </Link>
+        <Link to="/tenzies-game">
+          <img src={tenziesGameImg} alt="Tenzies Game section cover" />
+        </Link>
       </div>
     </section>
   );
